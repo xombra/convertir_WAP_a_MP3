0481ab1a8280b6b58b107166a4bc1d7fe03092b0
Hector A. Mantellini <xombra@users.noreply.github.com>
2015-10-16 10:58:00 -0430

    Script para cambiar de .wav a MP3

---
+++ lame.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SAVEIF=$IFS
+
+IFS=$(echo -en "\n\b")
+
+for file in $(ls *wav)
+do
+
+  name=${file%%.wav}
+  lame -V0 -h -b 160 --vbr-new $name.wav $name.mp3
+
+done
+
+IFS=$SAVEIFS
+exit 0
